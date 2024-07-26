0f0c02fe9fd86967847dbfc3820b15d21d409090
Hudaifa Abdullah ツ <loffy@live.com>
2024-07-26 19:10:13 +0300

    init

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DDNS_Cloudflare_API
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
+
